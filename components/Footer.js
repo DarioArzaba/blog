5098a7f862a394a5bfc5aa1d3451e50a56c3f0d1
DarioArzaba <dario.arzaba@gmail.com>
2021-05-16 14:29:42 -0500

    first commit

---
+++ components/Footer.js
@@ -0,0 +1,21 @@
+import Link from './Link'
+import siteMetadata from '@/data/siteMetadata'
+import SocialIcon from '@/components/social-icons'
+
+export default function Footer() {
+  return (
+    <footer>
+      <div className="flex flex-col items-center mt-16">
+        <div className="flex mb-2 space-x-2 text-sm text-gray-500 dark:text-gray-400">
+          <div>{siteMetadata.author}</div>
+          <div>{` • `}</div>
+          <div>{`${new Date().getFullYear()}`}</div>
+          <div>{` • `}</div>
+          <Link href="/">{siteMetadata.title}</Link>
+        </div>
+        <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
+        </div>
+      </div>
+    </footer>
+  )
+}
---
+++ data/projectsData.js
@@ -0,0 +1,18 @@
+const projectsData = [
+  {
+    title: 'Example Proyect External',
+    description: `Descripction of first proyect.`,
+    imgSrc: '/static/images/google.png',
+    href: 'https://www.google.com',
+  },
+  {
+    title: 'Example of proyect inside here as a post',
+    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
+    to the desired date and press "Go". No more worrying about lost keys or
+    forgotten handphones with this simple yet affordable solution.`,
+    imgSrc: '/static/images/time-machine.jpg',
+    href: '/blog/the-time-machine',
+  },
+]
+
+export default projectsData
