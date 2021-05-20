68e77b52ee2eb40649ca5324348e90d50b6c5c85
DarioArzaba <dario.arzaba@gmail.com>
2021-05-20 00:13:01 -0500

    Primeras Entradas de Poesia Correctas

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,15 +1,13 @@
 const projectsData = [
   {
-    title: 'Example Proyect External',
-    description: `Descripction of first proyect.`,
+    title: 'Example External Link',
+    description: `Test Description. This should send you to Google.`,
     imgSrc: '/static/images/google.png',
     href: 'https://www.google.com',
   },
   {
-    title: 'Example of proyect inside here as a post',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten handphones with this simple yet affordable solution.`,
+    title: 'Example',
+    description: `Testing description.`,
     imgSrc: '/static/images/time-machine.jpg',
     href: '/blog/the-time-machine',
   },
