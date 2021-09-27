af1c5b80ae09c4e04f1b3c0d4af9d23d83e50ffc
DarioArzaba <dario.arzaba@gmail.com>
2021-09-27 15:03:07 -0500

    Again trying to use this

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,16 +1,16 @@
 const projectsData = [
-  {
-    title: 'Example External Link',
-    description: `Test Description. This should send you to Google.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
-  },
-  {
-    title: 'Example',
-    description: `Testing description.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
-  },
+	{
+		title: 'Dario Arzaba Mosqueda',
+		description: `Arte y Escritos`,
+		imgSrc: '/static/images/time-machine.jpg',
+		href: '/blog/dario-arzaba-mosqueda-one',
+	},
+	{
+		title: 'Example External Link',
+		description: `Test Description. This should send you to Google.`,
+		imgSrc: '/static/images/google.png',
+		href: 'https://www.google.com',
+	}
 ]
 
 export default projectsData
