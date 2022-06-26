e5ac343c43a595fc89148487b2039d9463e66e78
Dario Arzaba <dario.arzaba@gmail.com>
2022-06-26 01:45:30 -0500

    Dummy Change to Trigger Env Vars

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 	enabled: process.env.ANALYZE === 'true',
 })
 
-// You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
 	default-src 'self';
 	style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
